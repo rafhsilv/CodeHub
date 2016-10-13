4054c8f6aa543c60d1b4789024a8d7c63e1cc7e9
rafhsilv <rafael.silva@ifood.com.br>
2016-10-13 19:04:28 -0300

    remove unused code

--- app/src/main/java/com/caij/codehub/interactor/imp/IssueInteractorImp.java
+++ app/src/main/java/com/caij/codehub/interactor/imp/IssueInteractorImp.java
@@ -11,29 +11,7 @@ import com.caij.lib.utils.VolleyManager;
 import com.caij.lib.volley.request.GsonRequest;
 import com.google.gson.reflect.TypeToken;
 
-/**
- * /10/31.
- */
+
 public class IssueInteractorImp implements IssueInteractor {
 
-    @Override
-    public void getIssue(String owner, String repo, String issueNumber, Object requestTag, final InteractorCallBack<Issue> interactorCallBack) {
-        interactorCallBack.onLoading();
-        String url = new StringBuilder().append(API.API_HOST).append("/repos/").append(owner)
-                .append("/").append(repo).append("/issues/")
-                .append(issueNumber).toString();
-        GsonRequest<Issue> request = new GsonRequest<Issue>(Request.Method.GET, url, new TypeToken<Issue>() {
-        }.getType(), new Response.Listener<Issue>() {
-                    @Override
-                    public void onResponse(Issue response) {
-                       interactorCallBack.onSuccess(response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                interactorCallBack.onError(error);
-            }
-        });
-        VolleyManager.addRequest(request, requestTag);
-    }
 }
